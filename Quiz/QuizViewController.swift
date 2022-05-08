c2e115a7c06692aad2493a1a162b922d6940917b
Sato Meri <satomeris@gmail.com>
2022-05-09 02:28:50 +0900

    Initial Commit

---
+++ Quiz/QuizViewController.swift
@@ -0,0 +1,82 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by litech on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    //クイズを格納する配列
+    var quizArray = [[Any]]()
+    
+    //正解数
+    var correctAnswer: Int = 0
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons1: UIButton!
+    @IBOutlet var choiceButtons2: UIButton!
+    @IBOutlet var choiceButtons3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //------------------------ここから下にクイズを書く------------------------//
+        
+        quizArray.append(["「食パン」は何の略でしょうか？", "食用パン", "給食用パン", "主食用パン", 3])
+        quizArray.append(["「合コン」は何の略でしょうか？", "合同コンパニー", "合同コンカティネイション", "合同コングリゲイション", 1])
+        quizArray.append(["「電卓」は何の略でしょうか？", "電子式卓上計算機", "電通式卓上計算機", "電気式卓上計算機", 1])
+        quizArray.append(["「切手」は何の略でしょうか？", "切取手形", "切符手形", "切札手形", 2])
+        quizArray.append(["「ボールペン」は何の略でしょうか？", "ボールポイントペン", "ボールインクペン", "ボールサインペン", 1])
+        
+        //------------------------ここから上にクイズを書く------------------------//
+        
+        //quizArrayの中身をシャッフルする
+        quizArray.shuffle()
+
+        choiceQuiz()
+    }
+    
+    func choiceQuiz() {
+        quizTextView.text = quizArray[0][0] as? String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        choiceButtons1.setTitle(quizArray[0][1] as? String, for: .normal)
+        choiceButtons2.setTitle(quizArray[0][2] as? String, for: .normal)
+        choiceButtons3.setTitle(quizArray[0][3] as? String, for: .normal)
+    }
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
+        if quizArray[0][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer+=1
+        }
+        
+        quizArray.remove(at: 0)
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if quizArray.count == 0 {
+            performSegueToResult()
+        } else {
+            choiceQuiz()
+        }
+    }
+    
+    func performSegueToResult() {
+        performSegue(withIdentifier: "toResultView", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toResultView") {
+            
+            let resultView = segue.destination as! ResultViewController
+            resultView.correctAnswer = self.correctAnswer
+        }
+    }
+}
+
+
